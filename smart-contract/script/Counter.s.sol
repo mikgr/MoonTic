183b45fb95e19ab6863703acb7b458540b755257
mikkel <m.gautier.roesen@gmail.com>
2026-02-28 10:41:18 +0100

    Add smart contract to proj to solution

---
+++ smart-contract/script/Counter.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+import {Ticket} from "../src/Ticket.sol";
+
+contract CounterScript is Script {
+    Ticket public ticketContract;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        uint256 fakeCheckOutBlockedTime = 1707321600;
+        uint256 venueOpenTime = 1772389800;          // Sun Mar 01 2026 18:30:00 GMT+0000
+        uint256 venueCloseTime = 1772405999;         // Sun Mar 01 2026 22:59:59 GMT+0000
+        uint64 totalTicketCount = 10;
+        string memory location = "Store VEGA, Enghavevej 40, 1674 Copenhagen V, Denmark";
+        
+        ticketContract = new Ticket(
+            fakeCheckOutBlockedTime,
+            venueOpenTime,
+            venueCloseTime,
+            totalTicketCount,
+            location
+        );
+    
+        vm.stopBroadcast();
+    }
+}
